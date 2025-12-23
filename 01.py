ce966c61cc9359f4a91752c5a44d69f59e80d99a
In Hwa Mo <33287574+inhwamo@users.noreply.github.com>
2025-12-23 00:30:33 -0800

    Got star number 1

--- 01.py
+++ 01.py
@@ -1,5 +1,3 @@
-from collections import * 
-
 # Dial from 0 to 99
 # L decreases the number. 
 # Dial starts at 50
@@ -7,7 +5,23 @@ from collections import *
 
 # 2 pieces of state:
 count = 0
-dial = 50
+dial_position = 50
 
+with open('01input.txt', 'r') as f:
+    for line in f:
+        line = line.strip()  # removes newline/whitespace
 
+        # Get letter and number from each line
+        letter = line[0] # first char is always L or R
+        number = int(line[1:]) # rest are 1 integer
 
+        if letter == "R":
+            dial_position = (dial_position + number) % 100
+        
+        elif letter == "L":
+            dial_position = (dial_position - number) % 100
+
+        if dial_position == 0:
+            count = count + 1
+
+print(count)
