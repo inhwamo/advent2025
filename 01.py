16de076ddc0f88b9ef01b5e9b3a474ba082cb7fd
In Hwa Mo <33287574+inhwamo@users.noreply.github.com>
2025-12-23 00:13:07 -0800

    start 01

---
+++ 01.py
@@ -0,0 +1,13 @@
+from collections import * 
+
+# Dial from 0 to 99
+# L decreases the number. 
+# Dial starts at 50
+# For example, L68 subtracts 50, then 18 more, which is 82.
+
+# 2 pieces of state:
+count = 0
+dial = 50
+
+
+
